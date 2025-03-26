522386db7f9958c5c92a425792a2012730205732
= <=>
2025-03-26 13:44:03 +0000

    cube

--- send-message-cube.js
+++ send-message-cube.js
@@ -1,8 +1,10 @@
 const { REST, Routes } = require('discord.js');
-const { channel_bonplan, channel_test,token } = require('./config.json');
+const { channel_spam, channel_test,token } = require('./config.json');
 
 const rest = new REST().setToken(token);
 let montext = { "content": "mon text" };
 montext.content = "Chaque semaine 1 cube 1* à récupérer ici :\nhttps://store.supercell.com/fr/clashroyale\nDites merci a tortugua ! pour le rappel";
-rest.post(Routes.channelMessages(channel_bonplan), { body: montext }).then(res=>{});
-console.log('Message Cube send');
+rest.post(Routes.channelMessages(channel_spam, { body: montext })).then(res=>{
+     console.log('Message Cube send');
+});
+
